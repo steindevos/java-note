dffed17359f5160b99b2e3d8a4967fd02e0e2f50
Vos <stein.vos@capgemini.com>
2019-01-03 22:02:53 +0100

    [ADD]: functionality to get specific note by id

--- src/main/java/com/capgemini/notitieApp/controller/NoteController.java
+++ src/main/java/com/capgemini/notitieApp/controller/NoteController.java
@@ -3,10 +3,10 @@ package com.capgemini.notitieApp.controller;
 import com.capgemini.notitieApp.model.Note;
 import com.capgemini.notitieApp.repository.NoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/notes/")
@@ -15,11 +15,20 @@ public class NoteController {
     @Autowired
     private NoteRepository noteRepository;
 
+    // Return all notes
     @RequestMapping(method = RequestMethod.GET)
     public Iterable<Note> getAll() {
         return noteRepository.findAll();
     }
 
+    // Return note by id
+    @RequestMapping(value = "{id}", method = RequestMethod.GET)
+    public Optional<Note> findById(@PathVariable long id) {
+        Optional<Note> result = noteRepository.findById(id);
+        return result;
+    }
+
+    // Post one note
     @RequestMapping(method = RequestMethod.POST)
     public Note postNote(@RequestBody Note newNote) {
         noteRepository.save(newNote);
