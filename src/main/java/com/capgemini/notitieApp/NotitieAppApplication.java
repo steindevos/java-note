8eab1de8c47509225192bf0b9e40df15fdae7fc7
Vos <stein.vos@capgemini.com>
2019-01-03 11:53:59 +0100

    initial commit

---
+++ src/main/java/com/capgemini/notitieApp/NotitieAppApplication.java
@@ -0,0 +1,14 @@
+package com.capgemini.notitieApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotitieAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotitieAppApplication.class, args);
+	}
+
+}
+
